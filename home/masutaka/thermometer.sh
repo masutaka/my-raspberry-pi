19adffefbcdb51864791b362301f8da02b95ce7e
Takashi Masuda <masutaka@feedforce.jp>
2015-08-03 21:04:04 +0900

    Change endpoint to feedforce

--- home/masutaka/thermometer.sh
+++ home/masutaka/thermometer.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 temperature=`/usr/local/bin/temper | awk -F, '{print $2}'`
-curl -F number=$temperature http://masutaka.net:5125/api/home/thermometer/temperature
+curl -F number=$temperature http://masutaka.net:5125/api/feedforce/thermometer/temperature
